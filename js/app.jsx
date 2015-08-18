e439a5e00508e9c0e9cb2c56d4d18d4e3590393b
Hana Fallisgaard <hfgaard@gmail.com>
2015-08-18 11:49:06 -0700

    react tutorial

---
+++ js/app.jsx
@@ -0,0 +1,4 @@
+var React = require('react');
+var CommentBox = require('./commentbox.jsx');
+
+React.render(<CommentBox url="comments.json" pollInterval={2000} />, document.getElementById('content'));
